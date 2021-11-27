a6d23b77df436658e28c8645cbe9f8729e54c8b4
Omer Kucuker <omerfaruk.kucuker@stu.fsm.edu.tr>
2021-11-27 11:40:34 +0300

    router changed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState }  from "react";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Favorites from "./components/Favorites.jsx";
 import Homepage from "./components/Homepage.jsx";
 
